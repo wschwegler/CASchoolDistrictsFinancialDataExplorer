aacd2aa9a61dde2d2cea271a09529a1494b52c74
Wilson Schwegler <schwegler@oxy.edu>
2024-11-25 22:58:13 -0800

    finished the continuation aspect
    finished the final step and combined the newly merged file with the originally merged file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from app.home import home_blueprint
 from app.page2 import page2_blueprint
 from app.page3 import page3_blueprint
+from app.page4 import page4_blueprint
 
 
 app = Flask(__name__)
@@ -10,9 +11,10 @@ app = Flask(__name__)
 app.config.from_object('config.Config')
 
 # Register Blueprints
-app.register_blueprint(home_blueprint)
+app.register_blueprint(home_blueprint, url_prefix='/home')
 app.register_blueprint(page2_blueprint, url_prefix='/page2')  # Added URL prefix for page2
 app.register_blueprint(page3_blueprint, url_prefix='/page3')
+app.register_blueprint(page4_blueprint, url_prefix='/page4')
 
 @app.route('/')
 def upload_form():
